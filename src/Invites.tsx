8f387a74a6e3555bb5a00dfc34b81ddeadb7fdd4
Damageddream <marcan.grab@gmail.com>
2023-09-16 18:41:27 +0200

    working on spliting invites into separate component

--- src/Invites.tsx
+++ src/Invites.tsx
@@ -4,13 +4,13 @@ import UserI, { UserWithInvites } from "./interfaces/userI";
 import { useDispatch, useSelector } from "react-redux";
 import { uiActions } from "./store/uiSlice";
 import { RootState } from "./store/store";
+import InviteSingle from "./components/InviteSingle";
 
 const Invites: React.FC = () => {
   const dispatch = useDispatch();
   const ui = useSelector((state: RootState) => state.ui);
-  const [answer, setAnswer] = useState<"accept" | "denie">("accept");
   const [invites, setInvites] = useState<UserI[]>();
-  const [inviteAnswered, setInviteAnswered] = useState(0);
+
   const token = localStorage.getItem("token");
 
   const getInvites = async () => {
@@ -33,61 +33,20 @@ const Invites: React.FC = () => {
     }
   };
 
-  const answerInvites = async (id: string) => {
-    dispatch(uiActions.startLoading())
-    const response = await fetch(serverUrl + "/users/invites", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token as string}`,
-      },
-      body: JSON.stringify({ answer, id }),
-    });
-    if (!response.ok) {
-      dispatch(uiActions.setError("Response to invite failed"));
-      dispatch(uiActions.endLoading())
-    }
-    if(response.ok){
-      setInviteAnswered(prev=>prev+1)
-      dispatch(uiActions.endLoading())
-    }
-  };
-  const clickAccpeptHandler = () => {
-    setAnswer("accept");
-  };
-  const clickDenieHandler = () => {
-    setAnswer("denie");
-  };
-
-  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const input = e.currentTarget[0] as HTMLInputElement;
-    if (input) {
-      const id = input.value;
-      try {
-        await answerInvites(id);
-      } catch (err) {
-        dispatch(uiActions.setError("Response to invite failed"));
-      }
-    } else {
-      dispatch(uiActions.setError("Wrong input"));
-    }
-  };
-
   useEffect(() => {
     getInvites().catch(() => {
       dispatch(uiActions.setError("Failed to fetch invites"));
     });
-  }, [inviteAnswered]);
+  }, []);
 
   return (
     <div>
-      <h1>Inviets: </h1>
+      <h1>Invites to friends: </h1>
       {invites && (
         <div>
           {invites.map((invite) => {
             return (
-
+              <InviteSingle id={invite._id} name={invite.name} photo={invite.photo} getInvites={()=>getInvites()} />
             );
           })}
         </div>
--- src/components/InviteSingle.tsx
+++ src/components/InviteSingle.tsx
@@ -1,8 +1,64 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../store/uiSlice";
+import { serverUrl } from "../utilities/URLs";
+import { RootState } from "../store/store";
+
 const InviteSingle: React.FC<{
   id: string;
   name: string;
   photo: string;
-}> = ({id,name, photo}) => {
+  getInvites: ()=>void
+
+}> = ({id,name, photo, getInvites}) => {
+  const [answer, setAnswer] = useState<"accept" | "denie">("accept");
+  const ui = useSelector((state: RootState) => state.ui);
+  const dispatch = useDispatch()
+  const clickAccpeptHandler = () => {
+    setAnswer("accept");
+  };
+  const clickDenieHandler = () => {
+    setAnswer("denie");
+  };
+
+  const answerInvites = async (id: string) => {
+    dispatch(uiActions.startLoading())
+    
+    
+    const token = localStorage.getItem("token");
+    const response = await fetch(serverUrl+ "/users/invites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token as string}`,
+      },
+      body: JSON.stringify({ answer, id }),
+    });
+    if (!response.ok) {
+      dispatch(uiActions.setError("Response to invite failed"));
+      dispatch(uiActions.endLoading())
+    }
+    if(response.ok){
+      getInvites()
+      dispatch(uiActions.endLoading())
+    }
+  };
+
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const input = e.currentTarget[0] as HTMLInputElement;
+    if (input) {
+      const id = input.value;
+      try {
+        await answerInvites(id);
+      } catch (err) {
+        dispatch(uiActions.setError("Response to invite failed"));
+      }
+    } else {
+      dispatch(uiActions.setError("Wrong input"));
+    }
+  };
+
   return (
     <>
       <div key={id}>
@@ -21,6 +77,7 @@ const InviteSingle: React.FC<{
           <button type="submit" onClick={clickDenieHandler}>
             decline
           </button>
+          {ui.loading && <div>Loading....</div>}
         </form>
       </div>
     </>
