9da504f42118ec9e6e40d37a9b875986b8b75f04
Damageddream <marcan.grab@gmail.com>
2023-09-18 19:32:34 +0200

    added loggin as guest custom hook. and login as guest from login page

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -5,15 +5,25 @@ import { useNavigate } from "react-router-dom";
 import { UserReduxI } from "./interfaces/userI";
 import { RootState } from "./store/store";
 import useCheckUser from "./customHooks/useCheckUser";
+import useGuestLogin from "./customHooks/useGuestLogin";
 
 const LogIn: React.FC = () => {
   useCheckUser()
+  const [guestLogin] = useGuestLogin()
   const navigate = useNavigate()
   const user: UserReduxI = useSelector((state: RootState)=> state.user)
 
   const facebook = () => {
     window.open(serverUrl + "/login/facebook", "_self");
   };
+
+  const handleClick = () => {
+    guestLogin().catch(() => {
+      console.error('er')
+    });
+  }
+
+
 
   useEffect(()=>{
     
@@ -27,6 +37,7 @@ const LogIn: React.FC = () => {
       <button onClick={facebook}>Login with facebook</button>
       <button onClick={()=>{navigate("/register")}}>Register user</button>
       <button onClick={()=>navigate("/login")}>Log In</button>
+      <button onClick={handleClick}>Visit as guest</button>
     </div>
   );
 };
---
+++ src/customHooks/useGuestLogin.ts
@@ -0,0 +1,39 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { userActions } from "../store/userSlice";
+import { serverUrl } from "../utilities/URLs";
+import { uiActions } from "../store/uiSlice";
+import { UserApiLoginObject } from "../interfaces/userI";
+
+export default function useGuestLogin() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const login = async () => {
+    dispatch(uiActions.startLoading());
+    const response = await fetch(serverUrl + "/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: "Guest", password: "guest987" }),
+    });
+    if (!response.ok) {
+      dispatch(uiActions.endLoading());
+      dispatch(uiActions.setError("Login failed"));
+      return;
+    }
+    if (response.ok) {
+      try {
+        const data = (await response.json()) as UserApiLoginObject;
+        dispatch(userActions.loggedIn(true));
+        dispatch(userActions.setUserInfo(data.user));
+        localStorage.setItem("token", data.token);
+        dispatch(uiActions.endLoading());
+        navigate("/wall");
+      } catch (err) {
+        dispatch(uiActions.setError(err));
+      }
+    }
+  };
+  return [login];
+}
