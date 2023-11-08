96c81341cf02dfa8dc6c62710545e03d2a08d389
unknown <marcan.grab@gmail.com>
2023-11-08 15:52:54 +0100

    working on testing card

--- src/__tests__/CommentTests/CommentCard.test.tsx
+++ src/__tests__/CommentTests/CommentCard.test.tsx
@@ -1,67 +1,79 @@
-import {screen, render } from '@testing-library/react'
+import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import CommentCard from '../../components/Comment/CommentCard';
+import CommentCard from "../../components/Comment/CommentCard";
 
-vi.mock('../../customHooks/useToggle', ()=>({
-    default: vi.fn(()=>[false, vi.fn()])
-}))
+vi.mock("../../customHooks/useToggle", () => ({
+  default: vi.fn().mockReturnValueOnce([vi.fn(), false]),
+}));
 
-vi.mock('../../components/Comment/CommentOptions', ()=>{
-    return {
-        default: ()=>{
-            return <div data-testid='commentOptions' />
-        }
-    }
-})
-vi.mock('../../components/Comment/EditComment', ()=>{
-    return {
-        default: ()=>{
-            return <div data-testid='commentEdit' />
-        }
-    }
-})
+vi.mock("../../components/Comment/CommentOptions", () => {
+  return {
+    default: () => {
+      return <div data-testid="commentOptions" />;
+    },
+  };
+});
+vi.mock("../../components/Comment/EditComment", () => {
+  return {
+    default: () => {
+      return <div data-testid="commentEdit" />;
+    },
+  };
+});
 
-const newLikeAddedMock = vi.fn()
+
+const newLikeAddedMock = vi.fn();
 
 const comment = {
-    _id: 123,
-    author: {
-        _id: 'authorId',
-        name: 'Author Name',
-        photo: 'path/photo',
-        friends: [],
-        invites: [],
-        invitesSent: [],
-    },
-    text: 'comment text',
-    likes: [],
-    timestamp: '00:00',
-    post: 'postId'
-}
+  _id: 123,
+  author: {
+    _id: "authorId",
+    name: "Author Name",
+    photo: "path/photo",
+    friends: [],
+    invites: [],
+    invitesSent: [],
+  },
+  text: "comment text",
+  likes: [],
+  timestamp: "00:00",
+  post: "postId",
+};
 
-describe('CommentCard component', ()=>{
-    it('renders card component details', ()=>{
-        render(<CommentCard comment={comment} newLikeAdded={newLikeAddedMock} />)
+describe("CommentCard component", () => {
+  it("renders card component details", () => {
+    render(<CommentCard comment={comment} newLikeAdded={newLikeAddedMock} />);
 
-        const authorPhoto = screen.getByAltText('author photo')
-        const authorName = screen.getByText('Author Name')
-        const commentText = screen.getByText(/comment text/i)
-        const likeIcon = screen.getByAltText('like icon')
+    const authorPhoto = screen.getByAltText("author photo");
+    const authorName = screen.getByText("Author Name");
+    const commentText = screen.getByText(/comment text/i);
+    const likeIcon = screen.getByAltText("like icon");
+
+    expect(authorPhoto).toBeInTheDocument();
+    expect(authorName).toBeInTheDocument();
+    expect(commentText).toBeInTheDocument();
+    expect(likeIcon).toBeInTheDocument();
+  }),
+    it("renders child components", () => {
+      render(<CommentCard comment={comment} newLikeAdded={newLikeAddedMock} />);
+
+      const commentOptions = screen.getByTestId("commentOptions");
+      const editComment = screen.queryByTestId("commentEdit");
+
+      expect(commentOptions).toBeInTheDocument();
+      expect(editComment).not.toBeInTheDocument();
     
-        expect(authorPhoto).toBeInTheDocument()
-        expect(authorName).toBeInTheDocument()
-        expect(commentText).toBeInTheDocument()
-        expect(likeIcon).toBeInTheDocument()
     }),
+    it("rendders edit Comment when toggle is true", () => {
+    //   vi.mock("../../customHooks/useToggle", () => ({
+    //     default: vi.fn(() => [true, vi.fn()]),
+    //   }));
 
-    it('renders child components', ()=>{
-        render(<CommentCard comment={comment} newLikeAdded={newLikeAddedMock} />)
+      render(<CommentCard comment={comment} newLikeAdded={newLikeAddedMock} />);
 
-        const commentOptions = screen.getByTestId('commentOptions')
-        const editComment = screen.queryByTestId('commentEdit')
+      const editComment = screen.queryByTestId("commentEdit");
 
-        expect(commentOptions).toBeInTheDocument()
-        expect(editComment).not.toBeInTheDocument()
-    })
-
-})
+      expect(editComment).toBeInTheDocument();
+  
+    });
+});
