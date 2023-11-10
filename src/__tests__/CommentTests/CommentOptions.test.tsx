2fc44a35dedda0d81629052513eef3bdf90e3446
unknown <marcan.grab@gmail.com>
2023-11-10 14:03:59 +0100

    working on implementing redux in unittest

--- src/__tests__/CommentTests/CommentOptions.test.tsx
+++ src/__tests__/CommentTests/CommentOptions.test.tsx
@@ -1,13 +1,34 @@
-import {screen, render } from '@testing-library/react';
+import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import CommentOptions from '../../components/Comment/CommentOptions';
+import CommentOptions from "../../components/Comment/CommentOptions";
+import { Provider } from "react-redux";
+import store from "../../store/store";
 
-
-vi.mock('../../customHooks/useToggle', ()=>({
-    default: vi.fn()
-}))
+vi.mock("../../customHooks/useOutsideClick", () => ({
+  current: document.createElement("div"),
+}));
 
 vi.mock("react-redux", () => ({
-    useDispatch: jest.fn(),
-    useSelector: jest.fn(),
-  }));
+  useDispatch: vi.fn(),
+  useSelector: vi.fn(),
+}));
+
+const mockProps = {
+  authorId: "id1",
+  commentId: "id2",
+  toggleShowEditComment: vi.fn(),
+};
+
+describe("CommentOptions component", () => {
+  it("renders comment options detials", () => {
+    render(
+      <Provider store={store}>
+        <CommentOptions {...mockProps} />
+      </Provider>
+    );
+
+    const dots = screen.getAllByAltText("three dots");
+
+    expect(dots).toBeInTheDocument();
+  });
+});
