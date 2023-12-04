b93673315fd743813d02c9ffbb10ebea57ebbcdb
unknown <marcan.grab@gmail.com>
2023-12-04 12:26:23 +0100

    added post.test

--- src/__tests__/PostTests/Post.test.tsx
+++ src/__tests__/PostTests/Post.test.tsx
@@ -9,44 +9,66 @@ import userEvent from "@testing-library/user-event";
 import { serverUrl } from "../../utilities/URLs";
 import { describe } from "vitest";
 
+vi.mock("../../components/Comment/AddComment", () => {
+  return {
+    default: () => {
+      return <div data-testid="addComment" />;
+    },
+  };
+});
+vi.mock("../../components/Comment/CommentCard", () => {
+  return {
+    default: () => {
+      return <div data-testid="commentCard" />;
+    },
+  };
+});
+vi.mock("../../components/Post/PostOptions", () => {
+  return {
+    default: () => {
+      return <div data-testid="postOptions" />;
+    },
+  };
+});
 
 const mockProps = {
-    postId: "1id"
-}
+  postId: "1id",
+};
 const response = {
-    text: "text",
+  text: "text",
+  author: {
+    name: "name",
+    photo: "path/photo",
+    _id: "authorId1",
+    friends: ["friends"],
+    invites: ["invite1", "invite2"],
+    invitesSent: ["inviteSent1", "inviteSent2"],
+  },
+  timestamp: "01.01.1900",
+  likes: ["1", "2"],
+  _id: 1,
+  comments: {
+    text: "textComment",
     author: {
-        name: "name",
-        photo: "path/photo",
-        _id: "authorId1",
-        friends: ["friends"],
-        invites: ["invite1", "invite2"],
-        invitesSent: ["inviteSent1", "inviteSent2"]
+      name: "nameCom",
+      photo: "path/photo/com",
+      _id: "authorId1Com",
+      friends: ["friendsCom"],
+      invites: ["invite1Com", "invite2Com"],
+      invitesSent: ["inviteSent1Com", "inviteSent2Com"],
     },
-    timestamp: "01.01.1900",
-    likes: ['1','2'],
-    _id: 1,
-    comments: {
-        text: "textComment",
-        author: {
-            name: "nameCom",
-            photo: "path/photo/com",
-            _id: "authorId1Com",
-            friends: ["friendsCom"],
-            invites: ["invite1Com", "invite2Com"],
-            invitesSent: ["inviteSent1Com", "inviteSent2Com"]
-        },
-        timestam: "02.02.1800",
-        likes: ['2com', '3com'],
-        _id:2,
-        post: "post1"
-    }
-  }
+    timestam: "02.02.1800",
+    likes: ["2com", "3com"],
+    _id: 2,
+    post: "post1",
+  },
+};
 
-
-
-const server = setupServer(http.post('path/post.com', () => {
-    return HttpResponse.json(response)}))
+const server = setupServer(
+  http.post("path/post.com", () => {
+    return HttpResponse.json(response);
+  })
+);
 
 // Start server before all tests
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
@@ -57,13 +79,35 @@ afterAll(() => server.close());
 // Reset handlers after each test `important for test isolation`
 afterEach(() => server.resetHandlers());
 
+describe("tests for Post component", () => {
+  it("checking if all details render correctly", () => {
+    renderWithProviders(<Post {...mockProps} />, {
+      preloadedState: initialState,
+    });
+    const postAuthorName = screen.getByText(/name/i);
+    const imgAvatar = screen.getByAltText("author photo");
+    const postText = screen.getByText(/text/i);
+    const postImage = screen.getByAltText(/image with post/i);
+    const likeIcon = screen.getByAltText("like icon");
+    const numberOfLikes = screen.getByText(response.likes.length.toString());
+    expect(postAuthorName).toBeInTheDocument();
+    expect(imgAvatar).toBeInTheDocument();
+    expect(postText).toBeInTheDocument();
+    expect(postImage).toBeInTheDocument();
+    expect(likeIcon).toBeInTheDocument();
+    expect(numberOfLikes).toBeInTheDocument();
+  }),
+    it("renders child components", () => {
+      renderWithProviders(<Post {...mockProps} />, {
+        preloadedState: initialState,
+      });
 
-describe("tests for Post component", ()=> {
-    it("checking if all details render correctly", ()=> {
-        renderWithProviders(<Post {...mockProps} />,{preloadedState: initialState}
-        )
-     const postAuthorName = screen.getByText(/name/i)
-     expect(postAuthorName).toBeInTheDocument()
-   
-    })
-})
+      const addComment = screen.getByTestId("addComment");
+      const commentCard = screen.getByTestId("commentCard");
+      const postOptions = screen.getByTestId("postOptions");
+
+      expect(addComment).toBeInTheDocument();
+      expect(commentCard).toBeInTheDocument();
+      expect(postOptions).toBeInTheDocument();
+    });
+});
